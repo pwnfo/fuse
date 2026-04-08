fec85ba79032881e7e39baf0a369d6c5422ed393
Ryan <pwnfo@proton.me>
2026-04-08 13:56:49 -0300

    fix(cli): ensure terminal state cleanup using finally

--- fuse/console.py
+++ fuse/console.py
@@ -54,6 +54,6 @@ def get_progress(e: Event, r: Any, total: int = 100) -> None:
             sleep(0.5)
         except KeyboardInterrupt:
             break
-
-    sys.stdout.write("\033[?25h\033[2K\r")
-    sys.stdout.flush()
+        finally:
+            sys.stdout.write("\033[?25h\033[2K\r")
+            sys.stdout.flush()
